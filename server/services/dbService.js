8b91bc1b9ca8a83995ac0fe3addf674278a05a2b
ryuker <lifeisagamenotaproduct@gmail.com>
2024-06-15 15:12:41 +0200

    modified database methods to support mysql2

--- server/services/dbService.js
+++ server/services/dbService.js
@@ -9,115 +9,113 @@ const db = await mysql.createConnection({
   database: 'app_db'
 })
 
-await db.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-  if (err) throw err
-  console.log('The solution is: ', rows[0].solution)
-})
-
-
-
+// query database example
+// const [rows, fields] = await db.execute(
+//   'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+//   ['Morty', 14]
+// );
 
 // database methods
 function getAllRows(table) {
   const query = `SELECT * FROM ${table}`;
 
   return new Promise((resolve) => {
-    db.all(query, [], (err, rows) => {
+    db.execute(query, [], (err, rows) => {
       if (err) resolve(err.message.red);
       resolve(rows); 
     });
   });
 }
 
-// function getRowById(table,id) {
-//   const query = `SELECT * FROM ${table} WHERE id = ${id}`;
+function getRowById(table,id) {
+  const query = `SELECT * FROM ${table} WHERE id = ${id}`;
 
-//   return new Promise((resolve) => {
-//     db.get(query, [], (err, rows) => {
-//       if (err) resolve(err.message.red);
-//       resolve(rows[0]); 
-//     });
-//   });
-// }
+  return new Promise((resolve) => {
+    db.execute(query, [], (err, rows) => {
+      if (err) resolve(err.message.red);
+      resolve(rows[0]); 
+    });
+  });
+}
 
-// function addRow(table, body) {
-//   const keys = Object.keys(body);
+function addRow(table, body) {
+  const keys = Object.keys(body);
   
-//   let params = "";
-//   keys.map((key, index) => {
-//     if (index == 0)
-//       params += `${key}`
-//     else
-//       params += `, ${key}`
-//   });
+  let params = "";
+  keys.map((key, index) => {
+    if (index == 0)
+      params += `${key}`
+    else
+      params += `, ${key}`
+  });
 
-//   let valuesStr = "";
-//   keys.map((key, index) => {
-//     if (index == 0)
-//       valuesStr += `?`
-//     else
-//       valuesStr += `,?`
-//   });
+  let valuesStr = "";
+  keys.map((key, index) => {
+    if (index == 0)
+      valuesStr += `?`
+    else
+      valuesStr += `,?`
+  });
 
-//   let paramValues = keys.map(key => body[key]);
+  let paramValues = keys.map(key => body[key]);
 
-//   // TODO add validation
+  // TODO add validation
 
-//   // Setup query
-//   const query = `
-//     INSERT INTO ${table}(${params}) 
-//     VALUES (${valuesStr})
-//   `;
+  // Setup query
+  const query = `
+    INSERT INTO ${table}(${params}) 
+    VALUES (${valuesStr})
+  `;
 
-//   return new Promise((resolve) => {
-//     db.run(query, paramValues, function(err) {
-//       if (err) resolve(err);
-//       resolve(this.lastID); 
-//     });
-//   });
-// }
+  return new Promise((resolve) => {
+    db.execute(query, paramValues, function(err) {
+      if (err) resolve(err);
+      resolve(this.lastID); 
+    });
+  });
+}
 
-// function updateRowById(table, id, body) {
-//   const columns = Object.entries(body);
+function updateRowById(table, id, body) {
+  const columns = Object.entries(body);
 
-//   let columnsStr = "";
+  let columnsStr = "";
 
-//   columns.map((column, index) => {
-//     columnsStr += `${column[0]}= "${column[1]}"`;
+  columns.map((column, index) => {
+    columnsStr += `${column[0]}= "${column[1]}"`;
     
-//     // add komma to any but the last column 
-//     if (columns.length > 1){
-//       if (index < columns.length -1)
-//         columnsStr += ',';
-//     }
-//   });
+    // add komma to any but the last column 
+    if (columns.length > 1){
+      if (index < columns.length -1)
+        columnsStr += ',';
+    }
+  });
 
-//   // Setup query
-//   const query = `UPDATE ${table} SET ${columnsStr} WHERE id = ?`;
+  // Setup query
+  const query = `UPDATE ${table} SET ${columnsStr} WHERE id = ?`;
 
-//   return new Promise((resolve) => {
-//     db.run(query, [
-//       id
-//     ], function(err) {
-//       if (err) resolve(err);
-//       resolve({id: id}); 
-//     });
-//   });
-// }
+  return new Promise((resolve) => {
+    db.execute(query, [
+      id
+    ], function(err) {
+      if (err) resolve(err);
+      resolve({id: id}); 
+    });
+  });
+}
 
-// function deleteRowById(table, id) {
-//   // Setup query
-//   const query = `DELETE FROM ${table} WHERE id = ?`;
+function deleteRowById(table, id) {
+  // Setup query
+  const query = `DELETE FROM ${table} WHERE id = ?`;
 
-//   return new Promise((resolve) => {
-//     db.run(query, [
-//       id
-//     ], function(err) {
-//       if (err) resolve(err);
-//       resolve({id: id}); 
-//     });
-//   });
-// }
+  return new Promise((resolve) => {
+    db.execute(query, [
+      id
+    ], function(err) {
+      if (err) resolve(err);
+      resolve({id: id}); 
+    });
+  });
+}
 
 module.exports = {
   getAllRows,
