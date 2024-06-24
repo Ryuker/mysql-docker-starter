c147b1f8b681b17eeead81c3d4e9eb9cbab0f6d0
ryuker <lifeisagamenotaproduct@gmail.com>
2024-06-24 19:34:17 +0200

    modified to use connect function from dbService as middleware

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,10 +7,10 @@ const {
   updateUserById,
   deleteUserById
 } = require('../controllers/users.js');
-const { db } = require('../services/dbService.js');
+const { connect } = require('../services/dbService.js');
 
 const router = express.Router();
-router.use(db);
+router.use(connect);
 
 // All Users
 router
